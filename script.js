c248fea63abddf2667c4fa78b7a117eb7bbc5bf1
Semandr <semandr.code@gmail.com>
2024-02-24 14:05:38 +0100

    find method

--- script.js
+++ script.js
@@ -336,9 +336,20 @@ calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 
+/*
 // Pipeline
 const totalDepositUSD = movements
   .filter((mov) => mov > 0)
   .map((mov) => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositUSD);
+
+*/
+
+const firstNegativ = movements.find((mov) => mov < 0);
+console.log(firstNegativ);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === 'Jessica Davis');
+console.log(account);
